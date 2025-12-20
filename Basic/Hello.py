ccbbe6cebb796f46daad9c519c755cf9797ba394
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2025-12-19 22:42:33 -0600

    Hola Mundo

---
+++ Basic/Hello.py
@@ -0,0 +1,26 @@
+# Esto es un comentario de una sola linea
+
+# Hola mundo
+print("Hola Python!")
+print('Hola Python!')
+
+"""
+Este es un
+comentario
+en varias lineas
+"""
+
+'''
+Este tambien es un
+comentario
+en varias lineas
+'''
+
+#Consultar el tipo de dato
+print(type("Soy un dato str")) # tipo 'str'
+print(type(5)) # tipo 'int'
+print(type(1.5)) # tipo 'float'
+print(type(True)) # tipo 'bool'
+print(type(1 + 1j)) # tipo 'complex'
+
+
